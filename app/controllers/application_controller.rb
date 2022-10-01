ca9096c9735ee67aea08e8cd3bec0bd70da9b4b1
薛羽 <xueyu@xueyudeMacBook-Pro.local>
2022-10-01 11:51:46 +0800

    add_name_to_users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+    # Prevent CSRF attacks by raising an exception
+    protect_from_forgery with: :exception
+    before_action :configure_permmited_parameters, if: :devise_controller?  
+    
+    protected
+
+    def configure_permmited_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    end
 end
